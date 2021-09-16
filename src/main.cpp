dc602d7287ba2117bfffbf088386e81d13a3259d
Ryoichi Ishikawa <ishikawa@cvl.iis.u-tokyo.ac.jp>
2021-09-16 23:32:11 +0900

    modify layered input

--- src/main.cpp
+++ src/main.cpp
@@ -18,13 +18,100 @@
 
 int main(int argc, char* argv[]) {
 	std::cout << "Kitty on your lap!" << std::endl;
-
+	//load config file
 	std::ifstream ifs(argv[1]);
 	nlohmann::json config;
 	ifs >> config;
 	ifs.close();
 	
-	std::string midiPath = config["midi"].get<std::string>();
+
+	bool bMidiDraw = false;
+	int vWidth = config["width"].get<int>();
+	int vHeight = config["height"].get<int>();
+
+
+	//load midi parameter
+	scoreVis sv;
+	smf::MidiFile midifile;
+	double duration = 0.0;
+	if (!config["midi"].is_null()) {
+		nlohmann::json config_midi = config["midi"];
+
+		bMidiDraw = true;
+		std::string midiPath = config_midi["file"].get<std::string>();
+		int mode = config_midi["mode"].get<int>();
+		double speed = config_midi["speed"].is_null() ? 1.0 : config_midi["speed"].get<double>();
+		
+		midifile.read(midiPath);
+		midifile.doTimeAnalysis();
+		midifile.linkNotePairs();
+		int minNote = 255;
+		int maxNote = 0;
+		for (int track = 0; track < midifile.getTrackCount(); ++track) {
+			for (int event = 0; event < midifile[track].size(); ++event) {
+				if (midifile[track][event].isNoteOn()) {
+					std::cout << midifile[track][event].seconds << std::endl;
+					int note = midifile[track][event][1];
+					if (note < minNote) {
+						minNote = note;
+					}
+					if (note > maxNote) {
+						maxNote = note;
+					}
+				}
+			}
+			std::cout << std::endl;
+		}
+
+
+		std::cout << "duration: " << midifile.getFileDurationInSeconds() << std::endl;
+		std::cout << "minNote: " << minNote << std::endl;
+		std::cout << "maxNote: " << maxNote << std::endl;
+		 duration = midifile.getFileDurationInSeconds();
+		double center = (minNote + maxNote)*0.5*0.05;
+		Eigen::Matrix3d R = Eigen::Matrix3d::Identity();
+		if (!config_midi["view"].is_null()) {
+			Eigen::Matrix3d Rx, Ry, Rz;
+			nlohmann::json viewp = config_midi["view"].get<nlohmann::json>();
+			Rx = Eigen::AngleAxisd(viewp["rx"].get<double>(), Eigen::Vector3d::UnitX());
+			Ry = Eigen::AngleAxisd(viewp["ry"].get<double>(), Eigen::Vector3d::UnitY());
+			Rz = Eigen::AngleAxisd(viewp["rz"].get<double>(), Eigen::Vector3d::UnitZ());
+			R = Rz * Ry* Rx;
+		}
+
+		if (mode == 0) {
+			double xpos = config_midi["x"].is_null() ? 0.0 : config_midi["x"].get<double>();
+			double ypos = config_midi["y"].is_null() ? center : center + config_midi["y"].get<double>();
+			double zpos = config_midi["z"].is_null() ? -1.0 : config_midi["z"].get<double>();
+			double zitv = config_midi["interval"].is_null() ? 0.1 : config_midi["interval"].get<double>();
+			sv.setMapping(new straightMapping(zitv));
+			sv.setRotation(R);
+			sv.setXYZ(xpos, ypos, zpos);
+		}
+		else if (mode == 1) {
+			double xpos = config_midi["x"].is_null() ? -1.0 : config_midi["x"].get<double>();
+			double ypos = config_midi["y"].is_null() ? 0.0 : config_midi["y"].get<double>();
+			double zpos = config_midi["z"].is_null() ? 0.0 : config_midi["z"].get<double>();
+			double zitv = config_midi["interval"].is_null() ? 0.1 : config_midi["interval"].get<double>();
+			double diag = config_midi["radius"].is_null() ? 1.0 : config_midi["radius"].get<double>();
+			double s_angle = config_midi["startangle"].is_null() ? 0.0 : config_midi["startangle"].get<double>();
+			double reso = config_midi["resolution"].is_null() ? 2 * M_PI / 127 : config_midi["resolution"].get<double>();
+
+			Eigen::Matrix3d Rdef;
+			Rdef = Eigen::AngleAxisd(M_PI / 2.0, Eigen::Vector3d::UnitY());
+			sv.setMapping(new circleMapping(zitv, diag, s_angle, reso));
+			sv.setRotation(Rdef*R);
+			sv.setXYZ(xpos, ypos, zpos);
+		}
+		sv.dur = duration;
+		sv.createContext(vWidth, vHeight);
+		sv.setSpeed(speed);
+
+	}
+
+
+	//load spectol parameter
+
 	std::string wavPath = config["wav"].get<std::string>();
 	
 	bool bSpec = true;
@@ -67,9 +154,7 @@ int main(int argc, char* argv[]) {
 		maxfreq_idx = maxfreq / (wav.sps() / n);
 	}
 	std::string outputPath = config["output"].get<std::string>();
-	int mode = config["mode"].get<int>();
-	int vWidth = config["width"].get<int>();
-	int vHeight = config["height"].get<int>();
+	
 	std::vector<std::string> encoding = config["encode_target"].get<std::vector<std::string>>();
 	if (encoding.size() == 0) {
 		std::cout << "ERROR: encode_target required!!" << std::endl;
@@ -80,95 +165,38 @@ int main(int argc, char* argv[]) {
 		vis = config["vis"].get<bool>();
 	}
 	bool setBG = false;
-	cv::Mat bg;
-	if (!config["bg"].is_null()) {//background image loading
-		std::string bgpath = config["bg"].get<std::string>();
-		bg = cv::imread(bgpath);
-		if (!bg.empty()) {
-			setBG = true;
-			cv::resize(bg,bg,cv::Size(vWidth,vHeight));
-		}
+
+	std::vector<nlohmann::json> sources;
+	if (!config["background"].is_null()) {//background image loading
+		sources = config["background"]["source"].get<std::vector<nlohmann::json>>();
+		setBG = true;
 	}
 
-
-	double speed = config["speed"].is_null() ?1.0 :config["speed"].get<double>();
-	smf::MidiFile midifile;
-	midifile.read(midiPath);
-	midifile.doTimeAnalysis();
-	midifile.linkNotePairs();
-	int minNote = 255;
-	int maxNote = 0;
-	for (int track = 0; track < midifile.getTrackCount(); ++track) {
-
-		for (int event = 0; event < midifile[track].size(); ++event) {
-			if (midifile[track][event].isNoteOn()) {
-				std::cout << midifile[track][event].seconds << std::endl;
-				int note = midifile[track][event][1];
-				if (note < minNote) {
-					minNote = note;
-				}
-				if (note > maxNote) {
-					maxNote = note;
-				}
+	std::vector<cv::Mat> bg(sources.size());
+	std::vector <double> dulations_bg(sources.size());
+	int bg_idx = 0;
+	if (setBG) {//background image loading
+		for (int i = 0; i < sources.size(); i++) {
+			
+			cv::resize(cv::imread(sources.at(i)["file"]), bg[i], cv::Size(vWidth, vHeight)); ;
+			if (!sources.at(i)["dulation"].is_null()) {
+				dulations_bg[i]=(sources.at(i)["dulation"].get<double>());
+			}
+			else {
+				dulations_bg[i] = (-1);
 			}
 		}
-
-		std::cout << std::endl;
 	}
+
+
+
+
+
+
 	
-	
-	std::cout << "duration: " << midifile.getFileDurationInSeconds() << std::endl;
-	std::cout << "minNote: " << minNote << std::endl;
-	std::cout << "maxNote: " << maxNote << std::endl;
-	double duration = midifile.getFileDurationInSeconds();
-	double center = (minNote + maxNote)*0.5*0.05;
-
-	Eigen::Matrix3d R = Eigen::Matrix3d::Identity();
-	if (!config["view"].is_null()) {
-		Eigen::Matrix3d Rx, Ry, Rz;
-		nlohmann::json viewp = config["view"].get<nlohmann::json>();
-		Rx = Eigen::AngleAxisd(viewp["rx"].get<double>(), Eigen::Vector3d::UnitX());
-		Ry = Eigen::AngleAxisd(viewp["ry"].get<double>(), Eigen::Vector3d::UnitY());
-		Rz = Eigen::AngleAxisd(viewp["rz"].get<double>(), Eigen::Vector3d::UnitZ());
-		R = Rz * Ry* Rx;
-	}
-
-	scoreVis sv;
-	if (mode == 0) {
-		double xpos = config["x"].is_null() ? 0.0 : config["x"].get<double>();
-		double ypos = config["y"].is_null() ? center : center + config["y"].get<double>();
-		double zpos = config["z"].is_null() ? -1.0 : config["z"].get<double>();
-		double zitv = config["interval"].is_null() ? 0.1 : config["interval"].get<double>();
-		sv.setMapping(new straightMapping(zitv));
-		sv.setRotation(R);
-		sv.setXYZ(xpos,ypos, zpos);
-	}
-	else if (mode == 1) {
-
-		double xpos = config["x"].is_null() ? -1.0 : config["x"].get<double>();
-		double ypos = config["y"].is_null() ? 0.0 : config["y"].get<double>();
-		double zpos = config["z"].is_null() ? 0.0 : config["z"].get<double>();
-		double zitv = config["interval"].is_null() ? 0.1 : config["interval"].get<double>();
-		double diag = config["radius"].is_null() ? 1.0 : config["radius"].get<double>();
-		double s_angle = config["startangle"].is_null() ? 0.0 : config["startangle"].get<double>();
-		double reso = config["resolution"].is_null() ? 2*M_PI/127 : config["resolution"].get<double>();
-
-		Eigen::Matrix3d Rdef;
-		Rdef = Eigen::AngleAxisd(M_PI/2.0, Eigen::Vector3d::UnitY());
-		sv.setMapping(new circleMapping(zitv, diag, s_angle, reso));
-		sv.setRotation(Rdef*R);
-		sv.setXYZ(xpos, ypos, zpos);
-	}
-	
-
-
-
-	sv.dur = duration;
-	sv.createContext(vWidth, vHeight);
 	GLubyte* buffer;
 	Eigen::Vector3d t;
 
-	sv.setSpeed(speed);
 	cv::VideoWriter writer;
 	writer.open("temp.mp4", cv::VideoWriter::fourcc('m', 'p', '4', 'v'), 30, cv::Size(vWidth, vHeight));
 	double fftbuffer[2 * n];
@@ -186,23 +214,41 @@ int main(int argc, char* argv[]) {
 			sv.setSpectrum(fftbuffer,n,minfreq_idx,maxfreq_idx);
 		}
 
-		sv.render(midifile, buffer, i);
-		cv::Mat colorimage = cv::Mat(cv::Size(vWidth, vHeight), CV_8UC3);
-		memcpy(colorimage.data, buffer, sizeof(uchar) * 3 * colorimage.size().width*colorimage.size().height);
-		cv::flip(colorimage, colorimage, 0);	
+		cv::Mat colorimage = cv::Mat(cv::Size(vWidth, vHeight), CV_8UC3, cv::Scalar(0));
+
+		if (bMidiDraw) {
+			sv.render(midifile, buffer, i);
+			memcpy(colorimage.data, buffer, sizeof(uchar) * 3 * colorimage.size().width*colorimage.size().height);
+			cv::flip(colorimage, colorimage, 0);
+		}
 		
 		//Background image blending
 		if (setBG) {
 			uchar* ci_ptr = colorimage.data;
-			uchar* bg_ptr = bg.data;
+			uchar* bg_ptr = bg.at(bg_idx).data;
+			uchar* bg_ptr_next = bg.at(bg_idx).data; //bg.at(bg_idx).data;
+			double blend_rate = 1.0;
+			if (dulations_bg.at(bg_idx) != -1 && dulations_bg.at(bg_idx) - i < 1.0) {
+				bg_ptr_next = bg.at(bg_idx+1).data;
+				blend_rate = dulations_bg.at(bg_idx) - i;
+				if (blend_rate < 0)blend_rate = 0;
+			}
+
 			int areasize = colorimage.size().area();
 			for (int pix = 0; pix < areasize; pix++) {
 				if (ci_ptr[pix * 3] == 0 && ci_ptr[pix * 3 + 1] == 0 && ci_ptr[pix * 3 + 2] == 0) {
-					ci_ptr[pix * 3] = bg_ptr[pix * 3];
-					ci_ptr[pix * 3 + 1] = bg_ptr[pix * 3 + 1];
-					ci_ptr[pix * 3 + 2] = bg_ptr[pix * 3 + 2];
+					ci_ptr[pix * 3] = blend_rate*bg_ptr[pix * 3] + 
+						(1.0- blend_rate) * bg_ptr_next[pix * 3];
+					ci_ptr[pix * 3 + 1] = blend_rate * bg_ptr[pix * 3 + 1] +
+						(1.0 - blend_rate) * bg_ptr_next[pix * 3 + 1];
+					ci_ptr[pix * 3 + 2] = blend_rate * bg_ptr[pix * 3 + 2] +
+						(1.0 - blend_rate) * bg_ptr_next[pix * 3 + 2];
 				}
 				//else: Todo alpha blending
+			}
+
+			if (dulations_bg.at(bg_idx) != -1 && dulations_bg.at(bg_idx) - i < 0.0) {
+				bg_idx++;
 			}
 		}
 		if (vis) {
